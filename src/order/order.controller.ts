e5577065a529bd2f56e861e07f86ef4147493ac4
sanjay-singh-patel <sanjupatel11112000@gmail.com>
2024-04-17 15:30:21 +0530

    initial commit

---
+++ src/order/order.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Post, Delete, Param, Body } from '@nestjs/common';
+import { OrderService } from './order.service';
+
+@Controller('orders')
+export class OrderController {
+  constructor(private readonly orderService: OrderService) {}
+
+  @Post()
+  async create(@Body() createOrderDto: any) {
+    return this.orderService.create(createOrderDto);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.orderService.findOne(id);
+  }
+
+  @Delete(':id')
+  async cancel(@Param('id') id: string) {
+    return this.orderService.cancel(id);
+  }
+}

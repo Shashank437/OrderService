6c7d43e5cc789409ccbf5467672a59fa40e0fe3a
kumar.shashank <kumar.shashank@ril.com>
2024-04-18 03:43:26 +0530

    added api's + env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrderModule } from './order/order.module';
+import 'dotenv/config';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/Order'),
-    OrderModule,
-  ],
+  imports: [MongooseModule.forRoot(process.env['MONGODB_URL']), OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Delete, Param, Body } from '@nestjs/common';
 import { OrderService } from './order.service';
 
-@Controller('orders')
+@Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
@@ -13,6 +13,11 @@ export class OrderController {
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return this.orderService.findOne(id);
+  }
+
+  @Get('/history/product/:id')
+  async getOrderHistory(@Param('id') id: string) {
+    return this.orderService.getProductOrderHistory(id);
   }
 
   @Delete(':id')
